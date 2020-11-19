5b51a7a95b698d398a43b3ca192f7363ae7120fb
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2020-11-19 13:37:40 +0530

    Code Files added

---
+++ 1. Setup ASP1.Net MVC Application/school_management.sql
@@ -0,0 +1,26 @@
+CREATE TABLE [dbo].[Student] (
+    [StudentID]      INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]       NVARCHAR (50) NULL,
+    [FirstName]      NVARCHAR (50) NULL,
+    [EnrollmentDate] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC)
+)
+
+CREATE TABLE [dbo].[Course] (
+    [CourseID] INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (50) NULL,
+    [Credits]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+)
+
+CREATE TABLE [dbo].[Enrollment] (
+    [EnrollmentID] INT IDENTITY (1, 1) NOT NULL,
+    [Grade]        DECIMAL(3, 2) NULL,
+    [CourseID]     INT NOT NULL,
+    [StudentID]    INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([EnrollmentID] ASC),
+    CONSTRAINT [FK_dbo.Enrollment_dbo.Course_CourseID] FOREIGN KEY ([CourseID]) 
+        REFERENCES [dbo].[Course] ([CourseID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Enrollment_dbo.Student_StudentID] FOREIGN KEY ([StudentID]) 
+        REFERENCES [dbo].[Student] ([StudentID]) ON DELETE CASCADE
+)
